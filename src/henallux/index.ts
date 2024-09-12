bf44205f7d50ea18a1366ddd1bd96c8634271f2e
Steven Sermeus <stevensermeus@gmail.com>
2024-09-12 14:48:41 +0200

    Add logging to fetch error

--- src/henallux/index.ts
+++ src/henallux/index.ts
@@ -8,15 +8,16 @@ export const getImplentations = unstable_cache(
 );
 
 async function INTERNAL_getImplentations() {
-  const response = await fetch(
-    "https://portail.henallux.be/api/implantations/my",
-    {
-      headers: {
-        Authorization: `Bearer ${env.BEARER_TOKEN}`,
-      },
-    },
-  );
   try {
+    const response = await fetch(
+      "https://portail.henallux.be/api/implantations/my",
+      {
+        headers: {
+          Authorization: `Bearer ${env.BEARER_TOKEN}`,
+        },
+      },
+    );
+
     const res = (await response.json()) as {
       success: number;
       data: {
@@ -48,66 +49,71 @@ async function INTERNAL_getImplentations() {
 }
 
 async function INTERNAL_getOrientations(implentation_id: number) {
-  const response = await fetch(
-    `https://portail.henallux.be/api/orientations/implantation/${implentation_id}`,
-    {
-      headers: {
-        Authorization: `Bearer ${env.BEARER_TOKEN}`,
+  try {
+    const response = await fetch(
+      `https://portail.henallux.be/api/orientations/implantation/${implentation_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.BEARER_TOKEN}`,
+        },
       },
-    },
-  );
-  const res = (await response.json()) as {
-    success: boolean;
-    data: {
-      id: number;
-      tri: number;
-      code_proeco_dep: string;
-      code: string;
-      code_unique: string;
-      code_ref: string;
-      codeeurope: number;
-      categorie: string;
-      sousect: string;
-      sousectexpansion: string;
-      sousectanglais: string;
-      intitule: string;
-      intitule_paysage: string;
-      intituleanglais: string;
-      type: string;
-      active: string;
-      international: string;
-      paysage: string;
-      bib: string;
-      idtech: number;
-      respcycle: string;
-      respimpl: string;
-      idDepartement: number;
-      valid_B1: string;
-      valid_B2: string;
-      valid_B3: string;
-      valid_B4: string;
-      nb_blocs: string;
-      code_ue_min: string;
-      debut: number;
-      fin: number;
-      id_cursus: string;
-      id_implantation: number;
-      key: number;
-      value: string;
-    }[];
-    count: number;
-    message: string;
-    code: number;
-  };
-
-  return res.data.map((orientation) => {
-    return {
-      key: orientation.key,
-      name: orientation.intitule,
-      code_ue_min: orientation.code_ue_min,
-      intitule_paysage: orientation.intitule_paysage,
+    );
+    const res = (await response.json()) as {
+      success: boolean;
+      data: {
+        id: number;
+        tri: number;
+        code_proeco_dep: string;
+        code: string;
+        code_unique: string;
+        code_ref: string;
+        codeeurope: number;
+        categorie: string;
+        sousect: string;
+        sousectexpansion: string;
+        sousectanglais: string;
+        intitule: string;
+        intitule_paysage: string;
+        intituleanglais: string;
+        type: string;
+        active: string;
+        international: string;
+        paysage: string;
+        bib: string;
+        idtech: number;
+        respcycle: string;
+        respimpl: string;
+        idDepartement: number;
+        valid_B1: string;
+        valid_B2: string;
+        valid_B3: string;
+        valid_B4: string;
+        nb_blocs: string;
+        code_ue_min: string;
+        debut: number;
+        fin: number;
+        id_cursus: string;
+        id_implantation: number;
+        key: number;
+        value: string;
+      }[];
+      count: number;
+      message: string;
+      code: number;
     };
-  });
+
+    return res.data.map((orientation) => {
+      return {
+        key: orientation.key,
+        name: orientation.intitule,
+        code_ue_min: orientation.code_ue_min,
+        intitule_paysage: orientation.intitule_paysage,
+      };
+    });
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
 export const getOrientations = unstable_cache(
@@ -120,45 +126,50 @@ async function INTERNAL_getYear(
   implentation_id: number,
   orientation_id: number,
 ) {
-  const response = await fetch(
-    `https://portail.henallux.be/api/classes/orientation_and_implantation/${orientation_id}/${implentation_id}`,
-    {
-      headers: {
-        Authorization: `Bearer ${env.BEARER_TOKEN}`,
+  try {
+    const response = await fetch(
+      `https://portail.henallux.be/api/classes/orientation_and_implantation/${orientation_id}/${implentation_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.BEARER_TOKEN}`,
+        },
       },
-    },
-  );
+    );
 
-  const res = (await response.json()) as {
-    success: boolean;
-    data: {
-      id: number;
-      promotion_id: string;
-      promotion_label: string;
-      implantation_id: string;
-      implantation_label: string;
-      orientation_id: string;
-      orientation_label: string;
-      annee: string;
-      classe: string;
-      code_hyperplanning: string;
-      id_hyperplanning: string;
-      created_at: string;
-      updated_at: string;
-      key: number;
-      value: string;
-    }[];
-    count: number;
-    message: string;
-    code: number;
-  };
-
-  return res.data.map((year) => {
-    return {
-      key: year.key,
-      name: year.promotion_label,
+    const res = (await response.json()) as {
+      success: boolean;
+      data: {
+        id: number;
+        promotion_id: string;
+        promotion_label: string;
+        implantation_id: string;
+        implantation_label: string;
+        orientation_id: string;
+        orientation_label: string;
+        annee: string;
+        classe: string;
+        code_hyperplanning: string;
+        id_hyperplanning: string;
+        created_at: string;
+        updated_at: string;
+        key: number;
+        value: string;
+      }[];
+      count: number;
+      message: string;
+      code: number;
     };
-  });
+
+    return res.data.map((year) => {
+      return {
+        key: year.key,
+        name: year.promotion_label,
+      };
+    });
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
 export const getYear = unstable_cache(INTERNAL_getYear, ["year"], {
@@ -170,44 +181,49 @@ async function INTERNAL_getClass(
   orientation_id: number,
   implantation_id: number,
 ) {
-  const response = await fetch(
-    `https://portail.henallux.be/api/classes/classe_and_orientation_and_implantation/${year_id}/${orientation_id}/${implantation_id}`,
-    {
-      headers: {
-        Authorization: `Bearer ${env.BEARER_TOKEN}`,
+  try {
+    const response = await fetch(
+      `https://portail.henallux.be/api/classes/classe_and_orientation_and_implantation/${year_id}/${orientation_id}/${implantation_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.BEARER_TOKEN}`,
+        },
       },
-    },
-  );
-  const res = (await response.json()) as {
-    success: boolean;
-    data: {
-      id: number;
-      promotion_id: string;
-      promotion_label: string;
-      implantation_id: string;
-      implantation_label: string;
-      orientation_id: string;
-      orientation_label: string;
-      annee: string;
-      classe: string;
-      code_hyperplanning: string;
-      id_hyperplanning: string;
-      created_at: string;
-      updated_at: string;
-      key: number;
-      value: string;
-    }[];
-    count: number;
-    message: string;
-    code: number;
-  };
-
-  return res.data.map((year) => {
-    return {
-      key: year.key,
-      name: year.promotion_label,
+    );
+    const res = (await response.json()) as {
+      success: boolean;
+      data: {
+        id: number;
+        promotion_id: string;
+        promotion_label: string;
+        implantation_id: string;
+        implantation_label: string;
+        orientation_id: string;
+        orientation_label: string;
+        annee: string;
+        classe: string;
+        code_hyperplanning: string;
+        id_hyperplanning: string;
+        created_at: string;
+        updated_at: string;
+        key: number;
+        value: string;
+      }[];
+      count: number;
+      message: string;
+      code: number;
     };
-  });
+
+    return res.data.map((year) => {
+      return {
+        key: year.key,
+        name: year.promotion_label,
+      };
+    });
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
 export const getClass = unstable_cache(INTERNAL_getClass, ["class"], {
@@ -215,40 +231,48 @@ export const getClass = unstable_cache(INTERNAL_getClass, ["class"], {
 });
 
 async function INTERNAL_getCourse(class_id: number) {
-  const response = await fetch(
-    `https://portail.henallux.be/api/plannings/promotion/[%22${class_id}%22]`,
-    {
-      headers: {
-        Authorization: `Bearer ${env.BEARER_TOKEN}`,
+  try {
+    const response = await fetch(
+      `https://portail.henallux.be/api/plannings/promotion/[%22${class_id}%22]`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.BEARER_TOKEN}`,
+        },
       },
-    },
-  );
+    );
 
-  const res = (await response.json()) as {
-    CATEGORIES: string;
-    created_date: string;
-    modified_date: string;
-    title: string;
-    start: string;
-    end: string;
-    text: string;
-    location: string;
-    details: string;
-    promotions: string;
-    "X-ALT-DESC;FMTTYPE=text/html": string;
-  }[];
-  const courses = res.map((r) => ({
-    location: r.location,
-    title: r.title,
-    start: r.start,
-    end: r.end,
-    promotions: r.promotions,
-    text: r.text.split(" - ")[0],
-  }));
-  return {
-    courses,
-    class_id,
-  };
+    const res = (await response.json()) as {
+      CATEGORIES: string;
+      created_date: string;
+      modified_date: string;
+      title: string;
+      start: string;
+      end: string;
+      text: string;
+      location: string;
+      details: string;
+      promotions: string;
+      "X-ALT-DESC;FMTTYPE=text/html": string;
+    }[];
+    const courses = res.map((r) => ({
+      location: r.location,
+      title: r.title,
+      start: r.start,
+      end: r.end,
+      promotions: r.promotions,
+      text: r.text.split(" - ")[0],
+    }));
+    return {
+      courses,
+      class_id,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      courses: [],
+      class_id,
+    };
+  }
 }
 
 export const getCourse = unstable_cache(INTERNAL_getCourse, ["course"], {
@@ -256,38 +280,43 @@ export const getCourse = unstable_cache(INTERNAL_getCourse, ["course"], {
 });
 
 async function INTERNAL_getLocaux(implentation_id: number) {
-  const response = await fetch(
-    `https://portail.henallux.be/api/locals/implantation/${implentation_id}`,
-    {
-      headers: {
-        Authorization: `Bearer ${env.BEARER_TOKEN}`,
+  try {
+    const response = await fetch(
+      `https://portail.henallux.be/api/locals/implantation/${implentation_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.BEARER_TOKEN}`,
+        },
       },
-    },
-  );
-  const res = (await response.json()) as {
-    success: boolean;
-    data: {
-      id: number;
-      id_hyperplanning: string;
-      local_name: string;
-      implantation_prefix: string;
-      implantation_name: string;
-      implantation_id: string;
-      created_at: string;
-      updated_at: string;
-      key: number;
-      value: string;
-    }[];
-    count: number;
-    message: string;
-    code: number;
-  };
-  return res.data.map((local) => {
-    return {
-      key: local.id,
-      name: local.local_name,
+    );
+    const res = (await response.json()) as {
+      success: boolean;
+      data: {
+        id: number;
+        id_hyperplanning: string;
+        local_name: string;
+        implantation_prefix: string;
+        implantation_name: string;
+        implantation_id: string;
+        created_at: string;
+        updated_at: string;
+        key: number;
+        value: string;
+      }[];
+      count: number;
+      message: string;
+      code: number;
     };
-  });
+    return res.data.map((local) => {
+      return {
+        key: local.id,
+        name: local.local_name,
+      };
+    });
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
 export const getLocaux = unstable_cache(INTERNAL_getLocaux, ["locaux"], {
@@ -295,59 +324,64 @@ export const getLocaux = unstable_cache(INTERNAL_getLocaux, ["locaux"], {
 });
 
 async function INTERNAL_getLocalSchedule(local_id: number, today: Date) {
-  const response = await fetch(
-    `https://portail.henallux.be/api/plannings/local/[%22${local_id}%22]`,
-    {
-      headers: {
-        Authorization: `Bearer ${env.BEARER_TOKEN}`,
+  try {
+    const response = await fetch(
+      `https://portail.henallux.be/api/plannings/local/[%22${local_id}%22]`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.BEARER_TOKEN}`,
+        },
       },
-    },
-  );
-  const res = (await response.json()) as {
-    CATEGORIES: string;
-    created_date: string;
-    modified_date: string;
-    title: string;
-    start?: string; //20240515T170000Z
-    end?: string; // 20240515T170000Z
-    text: string;
-    location: string;
-    details: string;
-    promotions: string;
-    "X-ALT-DESC;FMTTYPE=text/html": string;
-  }[];
-
-  const schedule = res.map((r) => ({
-    title: r.title,
-    start: r.start
-      ? new Date(
-          r.start.replace(
-            /(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z/,
-            "$1-$2-$3T$4:$5:$6Z",
-          ),
-        )
-      : null,
-    end: r.end
-      ? new Date(
-          r.end.replace(
-            /(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z/,
-            "$1-$2-$3T$4:$5:$6Z",
-          ),
-        )
-      : null,
-    location: r.location,
-    text: r.text,
-  }));
-  return schedule.filter((s) => {
-    if (!s.start || !s.end) {
-      return false;
-    }
-    return (
-      s.start.getDate() === today.getDate() &&
-      s.start.getMonth() === today.getMonth() &&
-      s.start.getFullYear() === today.getFullYear()
     );
-  });
+    const res = (await response.json()) as {
+      CATEGORIES: string;
+      created_date: string;
+      modified_date: string;
+      title: string;
+      start?: string; //20240515T170000Z
+      end?: string; // 20240515T170000Z
+      text: string;
+      location: string;
+      details: string;
+      promotions: string;
+      "X-ALT-DESC;FMTTYPE=text/html": string;
+    }[];
+
+    const schedule = res.map((r) => ({
+      title: r.title,
+      start: r.start
+        ? new Date(
+            r.start.replace(
+              /(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z/,
+              "$1-$2-$3T$4:$5:$6Z",
+            ),
+          )
+        : null,
+      end: r.end
+        ? new Date(
+            r.end.replace(
+              /(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z/,
+              "$1-$2-$3T$4:$5:$6Z",
+            ),
+          )
+        : null,
+      location: r.location,
+      text: r.text,
+    }));
+    return schedule.filter((s) => {
+      if (!s.start || !s.end) {
+        return false;
+      }
+      return (
+        s.start.getDate() === today.getDate() &&
+        s.start.getMonth() === today.getMonth() &&
+        s.start.getFullYear() === today.getFullYear()
+      );
+    });
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 }
 
 export const getLocalSchedule = unstable_cache(
